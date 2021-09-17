8a09816f4edbc81a08b98726515b595e609cfe93
Valeriy Malyshev <v.malyshev@piogroup.net>
2021-09-17 10:13:55 +0300

    2 lesson completed

--- sources/src/Controller/NewController.php
+++ sources/src/Controller/NewController.php
@@ -3,18 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewController extends AbstractController
 {
     /**
-     * @Route("/new", name="new")
+     * @Route("/new/{name}", name="new")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $value = 'my new value';
+
+        $routeName = $request->attributes->get('_route');
+        $routeParameters = $request->attributes->get('_route_params');
+
+        $listShowPage = $this->generateUrl('new', [
+            'name' => 'check',
+        ]);
+
         return $this->render('new/index.html.twig', [
-            'controller_name' => 'NewController',
+            'value' => $value,
+            'routeName' => $routeName,
+            'routeParameters' => $routeParameters,
         ]);
     }
 }
